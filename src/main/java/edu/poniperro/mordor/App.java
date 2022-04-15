ed15e6785a3e65fe4d6463d2d92ab0b453fb0c1c
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-04-15 17:05:00 +0200

    refactor: Descomentar código en el main

--- src/main/java/edu/poniperro/mordor/App.java
+++ src/main/java/edu/poniperro/mordor/App.java
@@ -5,7 +5,7 @@ import edu.poniperro.mordor.interfaces.PedidoPeligroso;
 import edu.poniperro.mordor.interfaces.TratamientoPedido;
 import edu.poniperro.mordor.pedidos.PedidoInternacional;
 import edu.poniperro.mordor.pedidos.PedidoPeligrosoOrden;
-//import edu.poniperro.mordor.procesadores.Oficina;
+import edu.poniperro.mordor.procesadores.Oficina;
 import edu.poniperro.mordor.tratamientos.TratamientoPedidoInternacional;
 import edu.poniperro.mordor.tratamientos.TratamientoPedidoPeligroso;
 
@@ -49,30 +49,30 @@ public class App {
 
     public static void main(String[] args) {
 
-//        Oficina oficina = new Oficina();
+        Oficina oficina = new Oficina();
 
         Pedido pedido = new PedidoInternacional("Comarca", 10);
         TratamientoPedido tratamientoInt = new TratamientoPedidoInternacional(
                 (PedidoInternacional) pedido);
 
-//        System.out.println(oficina.printarStatus(oficina.procesa(tratamientoInt), pedido));
+        System.out.println(oficina.printarStatus(oficina.procesa(tratamientoInt), pedido));
 
         pedido = new PedidoInternacional("Mordor", 10);
         tratamientoInt = new TratamientoPedidoInternacional((PedidoInternacional) pedido);
 
-//        System.out.println(oficina.printarStatus(oficina.procesa(tratamientoInt), pedido));
+        System.out.println(oficina.printarStatus(oficina.procesa(tratamientoInt), pedido));
 
         pedido = new PedidoPeligrosoOrden("Cima de los vientos",
                 "No urgarse en las uñas con este puñal");
         TratamientoPedido peligroso = new TratamientoPedidoPeligroso((PedidoPeligroso) pedido);
 
-//        System.out.println(oficina.printarStatus(oficina.procesa(peligroso), pedido));
+        System.out.println(oficina.printarStatus(oficina.procesa(peligroso), pedido));
 
         pedido = new PedidoPeligrosoOrden("Monte del destino",
                 "No ponerselo en el dedo");
         peligroso = new TratamientoPedidoPeligroso((PedidoPeligroso) pedido);
 
-//        System.out.println(oficina.printarStatus(oficina.procesa(peligroso), pedido));
+        System.out.println(oficina.printarStatus(oficina.procesa(peligroso), pedido));
 
         /**
          * Los pedidos multiples se completan en el ultimo de los casos test

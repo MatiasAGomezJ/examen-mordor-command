5253c41e873e1d71fec699eabef87a04f851025c
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-04-15 15:17:54 +0200

    Initial commit

---
+++ src/test/java/edu/poniperro/mordor/AppTest.java
@@ -0,0 +1,14 @@
+package edu.poniperro.mordor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
